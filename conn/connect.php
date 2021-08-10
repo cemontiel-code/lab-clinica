4609b7f9e4726edb404320797abfdd9cfaf4d474
Carlos Eduardo Montiel <78514261+cemontiel-code@users.noreply.github.com>
2021-08-09 22:12:40 -0400

    Add files via upload

---
+++ conn/connect.php
@@ -0,0 +1,14 @@
+<?php
+$dbHost = "localhost";
+$dbUser = 'root';
+$dbPassword = '';
+$dBase = 'clinica_lab';
+
+$con = mysqli_connect($dbHost,$dbUser,$dbPassword,$dBase);
+// Check connection
+if(mysqli_connect_errno()){
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+ 
+
+?>
